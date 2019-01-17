3896beedabe4c000cb3d29843961c7d1adf32c10
protpolymer <nvsvkont@gmail.com>
2019-01-17 18:26:50 +0300

    git initialization

---
+++ src/main/java/ru/itpark/mcdonaldsweb/McDonaldsWebApplication.java
@@ -0,0 +1,14 @@
+package ru.itpark.mcdonaldsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McDonaldsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McDonaldsWebApplication.class, args);
+    }
+
+}
+
